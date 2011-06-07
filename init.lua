ad185179988cecdbdb626f6721a4be16feb998ad
Jesse R. Adams <jesse@techno-geeks.org>
2011-06-06 20:59:25 -0500

    Handle error case when there is no internet connectivity

--- init.lua
+++ init.lua
@@ -21,35 +21,37 @@ function update(widget, postal_code, data_points)
   local data = http.request(url)
 
   -- Only snag current conditions
-  current = data:match("<current_conditions>(.-)</current_conditions>")
+  if data then
+    current = data:match("<current_conditions>(.-)</current_conditions>")
 
-  -- Data point config
-  if not data_points then
-    data_points = { "condition", "temp_f", "temp_c", "wind_condition", "humidity" }
-  end
-
-  -- Parse!
-  local parsed_values = {}
-  for key,name in ipairs(data_points) do
-    parsed_value = current:match('<' .. name ..' data="(.-)"/>')
-    parsed_values[name] = parsed_value
-  end
-
-  -- Set Text
-  widget.text = " " .. parsed_values["temp_f"]  .. "o F  "
-
-  -- Set Tooltip
-  tip = "\n"
-  for k,v in pairs(parsed_values) do
-    tip = tip .. k .. ": " .. v .. "\n"
-  end
-
-  tooltip = awful.tooltip({
-    objects = { widget },
-    timer_function = function()
-      return tip
+    -- Data point config
+    if not data_points then
+      data_points = { "condition", "temp_f", "temp_c", "wind_condition", "humidity" }
     end
-  })
+
+    -- Parse!
+    local parsed_values = {}
+    for key,name in ipairs(data_points) do
+      parsed_value = current:match('<' .. name ..' data="(.-)"/>')
+      parsed_values[name] = parsed_value
+    end
+
+    -- Set Text
+    widget.text = " " .. parsed_values["temp_f"]  .. "o F  "
+
+    -- Set Tooltip
+    tip = "\n"
+    for k,v in pairs(parsed_values) do
+      tip = tip .. k .. ": " .. v .. "\n"
+    end
+
+    tooltip = awful.tooltip({
+      objects = { widget },
+      timer_function = function()
+        return tip
+      end
+    })
+  end
 end
 
 function register(widget, postal_code, data_points)
